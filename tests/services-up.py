10addb3e36db2b5523be8491c58b149af8460f6c
Eddy ZAGABE <zagabe.ed@gmail.com>
2017-01-21 19:07:05 +0100

    test: services up scripts enhancement (retires, failed and continue)

--- tests/services-up.py
+++ tests/services-up.py
@@ -13,13 +13,26 @@ def ping(ip, port):
 
 
 idx = 1
+retries = 5
 while idx < len(sys.argv) - 1:
     print "Waiting for "+sys.argv[idx]+" to launch..."
 
-    while ping("localhost", sys.argv[idx+1]) != 0:
+    count = 0
+    while count < retries and ping("localhost", sys.argv[idx+1]) != 0:
+        print "Retrying for "+sys.argv[idx]+ " ..."
         time.sleep(1)
+        count += 1
+        
+
+        
+    if count == retries:
+        print "Unable to reach "+sys.argv[idx]
+        print ""
+        idx = idx + 2
+        continue
 
     print sys.argv[idx]+ " is up and running"
     print ""
-        
+
     idx = idx + 2
+
